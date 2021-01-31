a41c9d1b42a245f9fc1b6c734589f6910e3372fd
Kane Knight <kane.knight@hotmail.com>
2021-01-31 02:50:20 +0000

    url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http://localhost'),
+  url: env('URL'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '293f76311d2747966964f89f9cdcedc2'),
